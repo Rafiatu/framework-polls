e16c06e01212281361ac9944f3cf3b6ac2592798
Rafihatu Bello <61936161+Rafiatu@users.noreply.github.com>
2020-10-21 15:47:38 +0100

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -6,8 +6,8 @@ from rest_framework.authtoken import views
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
 urlpatterns = [
-    path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+#     path("polls/", PollList.as_view(), name="polls_list"),
+#     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
